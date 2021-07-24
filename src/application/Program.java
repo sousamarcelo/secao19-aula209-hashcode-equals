19f7dc1660220a04d81c139cc40efee7951f32bc
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-24 18:20:27 -0300

    criado a classe Client com hashCode + comparando por atributos dos objetos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,17 @@
 package application;
 
+import model.entity.Client;
+
 public class Program {
 	
 	public static void main(String[] args) {
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Pedro", "pedro@gmail.com");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
 		
 	}
 
