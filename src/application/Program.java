6a0a915a1262892b507d69fa0794b7e6360a0766
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-24 18:29:13 -0300

    Comparando objetos, por terem posição diferentes no heap = fals

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,11 +7,19 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Client c1 = new Client("Maria", "maria@gmail.com");
-		Client c2 = new Client("Pedro", "pedro@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
 		
 		System.out.println(c1.hashCode());
 		System.out.println(c2.hashCode());
-		System.out.println(c1.equals(c2));
+		System.out.println("Comparando por atributos: " + c1.equals(c2));
+		
+/* 		comparando os objetos e não os atributos, nessse caso o equals da falso, pois cada
+		objeto tem sua posição no heap.
+		utilizando em casos que todos os atributos são igual, o objeto nunca sera o mesmo.
+ */		
+		System.out.print("Comparando objetos: ");
+		System.out.println(c1 == c2);
 		
 	}
 
